735480eaf7ec9f761ef9eba1f373e2a879a351c5
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-11 04:12:22 +0300

    Renaming

--- amle.py
+++ amle.py
@@ -42,39 +42,39 @@ def amle_inpainting(input_m, mask, fidelity, tolerance, max_iter, d_t):
     # Derivatives Kernels
     k_a_i, k_a_j, k_b_i, k_b_j, k_c_i, k_c_j = create_kernel_derivatives()
 
-    input_copy = input_m.copy()
+    u = input_m.copy()
     vec = scipy.zeros((M, N, 2))
 
     for c_iter in range(0, C):
         for iteration in range(0, max_iter):
-            input_copy_x = cv.filter2D(input_copy[:, :, c_iter], -1, k_a_i)
-            input_copy_y = cv.filter2D(input_copy[:, :, c_iter], -1, k_a_j)
+            u_x = cv.filter2D(u[:, :, c_iter], -1, k_a_i)
+            u_y = cv.filter2D(u[:, :, c_iter], -1, k_a_j)
 
-            input_copy_xx = cv.filter2D(input_copy_x, -1, k_b_i)
-            input_copy_xy = cv.filter2D(input_copy_x, -1, k_b_j)
-            input_copy_yx = cv.filter2D(input_copy_y, -1, k_b_i)
-            input_copy_yy = cv.filter2D(input_copy_y, -1, k_b_j)
+            u_xx = cv.filter2D(u_x, -1, k_b_i)
+            u_xy = cv.filter2D(u_x, -1, k_b_j)
+            u_yx = cv.filter2D(u_y, -1, k_b_i)
+            u_yy = cv.filter2D(u_y, -1, k_b_j)
 
-            vec[:, :, 0] = cv.filter2D(input_copy[:, :, c_iter], -1, k_c_i)
-            vec[:, :, 1] = cv.filter2D(input_copy[:, :, c_iter], -1, k_c_j)
+            vec[:, :, 0] = cv.filter2D(u[:, :, c_iter], -1, k_c_i)
+            vec[:, :, 1] = cv.filter2D(u[:, :, c_iter], -1, k_c_j)
 
-            new_input_copy = input_copy[:, :, c_iter] + d_t * (
-                input_copy_xx * vec[:, :, 0]**2 + input_copy_yy * vec[:, :, 1]**2 + (input_copy_xy + input_copy_yx) *
+            new_u = u[:, :, c_iter] + d_t * (
+                u_xx * vec[:, :, 0]**2 + u_yy * vec[:, :, 1]**2 + (u_xy + u_yx) *
                 (vec[:, :, 0] * vec[:, :, 1]) + fidelity * mask[:, :, c_iter] *
-                (input_m[:, :, c_iter] - input_copy[:, :, c_iter]))
+                (input_m[:, :, c_iter] - u[:, :, c_iter]))
 
             diff_input = np.linalg.norm(
-                new_input_copy.reshape(M * N, 1) - input_copy[:, :, c_iter].reshape(M * N, 1),
-                2) / np.linalg.norm(new_input_copy.reshape(M * N, 1), 2)
+                new_u.reshape(M * N, 1) - u[:, :, c_iter].reshape(M * N, 1),
+                2) / np.linalg.norm(new_u.reshape(M * N, 1), 2)
 
-            input_copy[:, :, c_iter] = new_input_copy
+            u[:, :, c_iter] = new_u
 
             if diff_input < tolerance:
                 break
 
     if C == 1:
-        mpimage.imsave("outuput_ample.png", input_copy[:, :, 0], cmap="gray")
+        mpimage.imsave("outuput_ample.png", u[:, :, 0], cmap="gray")
     elif C == 3:
-        mpimage.imsave("outuput_ample.png", input_copy)
+        mpimage.imsave("outuput_ample.png", u)
 
-    return input_copy
+    return u
